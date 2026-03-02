9eaeb17d5b51812a4451e058aab83557ed0172e4
mdislamrakibul <rakibul.nexkraft@gmail.com>
2026-03-01 23:28:42 -0500

    about us page fix

--- resources/views/About_Us.blade.php
+++ resources/views/About_Us.blade.php
@@ -18,7 +18,8 @@
         <div class="container pt-25">
             <div class="row">
                 <div class="col-lg-6 align-self-center mb-lg-0 mb-4">
-                    <h6 class="mt-0 mb-15 text-uppercase font-sm text-brand wow fadeIn animated">Apon Plastic Industries</h6>
+                    <h6 class="mt-0 mb-15 text-uppercase font-sm text-brand wow fadeIn animated">Apon Plastic Industries
+                    </h6>
                     <h1 class="font-heading mb-40">
                         Creating complex products from concept to completion
                     </h1>
@@ -32,6 +33,11 @@
                         name reflects our unwavering commitment to excellence. We continuously strive to
                         exceed customer expectations, not just in the products we deliver, but also in the
                         level of service and support we provide. </p>
+                    <p>
+                        Not sure which manufacturing process is best for your complex molded component?
+                        Let us handle it. We can advise you on the best molding process for your project
+                        based on your product design, performance requirements, and budget.
+                    </p>
                 </div>
                 <div class="col-lg-6">
                     <img src="assets/imgs/page/about-1.png" alt="">
@@ -44,10 +50,8 @@
             <div class="row mb-50">
                 <div class="col-lg-12 col-md-12 text-center">
                     <h6 class="mt-0 mb-10 text-uppercase  text-brand font-sm wow fadeIn animated">some facts</h6>
-                    <h2 class="mb-15 text-grey-1 wow fadeIn animated">Take a look what<br> our clients say about us</h2>
-                    <p class="w-50 m-auto text-grey-3 wow fadeIn animated">At vero eos et accusamus et iusto odio
-                        dignissimos ducimus quiblanditiis praesentium. ebitis nesciunt voluptatum dicta reprehenderit
-                        accusamus</p>
+                    <h2 class="mb-15 text-grey-1 wow fadeIn animated">Take a look on<br> Company Management</h2>
+
                 </div>
             </div>
             <div class="row align-items-center">
@@ -55,15 +59,24 @@
                     <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
                         <div class="hero-card-icon icon-left-2 hover-up ">
                             <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-1.jpg" alt="">
+                                src="{{ asset('assets/uploads/About Us/Anisuz ZamanChairman.png') }}"
+                                alt="">
+
+                            {{-- <img class="animated slider-1-1"
+                                src="{{ asset('assets/uploads/Main Slider Design/Main Slider Design-2-Apon Plastic.png') }}"
+                                alt=""> --}}
                         </div>
                         <div class="pl-30">
                             <h5 class="mb-5 fw-500">
-                                J. Bezos
+                                Anisuz Zaman
                             </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
+                            <p class="font-sm text-grey-5">Chairman</p>
+                            <p class="text-grey-3" style="text-align: justify">With over 39 years of experience, our
+                                Chairman is a seasoned
+                                leader in the industry, known for his expertise and dedication. His pivotal
+                                role in establishing Apon Plastic's reputation for excellence speaks volumes
+                                about his commitment to quality and innovation.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -71,15 +84,21 @@
                     <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
                         <div class="hero-card-icon icon-left-2 hover-up ">
                             <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-3.jpg" alt="">
+                                src="{{ asset('assets/uploads/About Us/Paul ZamanManaging Director.png') }}"
+                                alt="">
                         </div>
                         <div class="pl-30">
                             <h5 class="mb-5 fw-500">
-                                B.Gates
+                                Paul Zaman
                             </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
+                            <p class="font-sm text-grey-5">Managing Director</p>
+                            <p class="text-grey-3" style="text-align: justify">Our Managing Director, the son of our
+                                esteemed Chairman,
+                                brings a fresh perspective and dedication to our company. Having learned
+                                the intricacies of injection molding and plastics directly from the Chairman,
+                                coupled with the completion of RJG courses, he's equipped with a solid
+                                foundation.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -87,66 +106,26 @@
                     <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
                         <div class="hero-card-icon icon-left-2 hover-up ">
                             <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-2.jpg" alt="">
+                                src="{{ asset('assets/uploads/About Us/Maruf HassanSales and Marketing Manager.png') }}"
+                                alt="">
                         </div>
                         <div class="pl-30">
                             <h5 class="mb-5 fw-500">
-                                B. Meyers
+                                Maruf Hassan
+
                             </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
+                            <p class="font-sm text-grey-5">Sales and Marketing Manager</p>
+                            <p class="text-grey-3" style="text-align: justify">Mr. Maruf Hassan's expertise has been
+                                pivotal in driving
+                                our growth and development here in Bangladesh. With over 7 years of specialized
+                                experience in Sales and
+                                a total of 10 years immersed in the plastic sector, Mr. Maruf brings a wealth of
+                                industry knowledge to
+                                our team.</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
-                        <div class="hero-card-icon icon-left-2 hover-up ">
-                            <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-4.jpg" alt="">
-                        </div>
-                        <div class="pl-30">
-                            <h5 class="mb-5 fw-500">
-                                J. Bezos
-                            </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
-                        <div class="hero-card-icon icon-left-2 hover-up ">
-                            <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-5.jpg" alt="">
-                        </div>
-                        <div class="pl-30">
-                            <h5 class="mb-5 fw-500">
-                                B.Gates
-                            </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4">
-                    <div class="hero-card box-shadow-outer-6 wow fadeIn animated mb-30 hover-up d-flex">
-                        <div class="hero-card-icon icon-left-2 hover-up ">
-                            <img class="btn-shadow-brand hover-up border-radius-5 bg-brand-muted"
-                                src="assets/imgs/page/avatar-1.jpg" alt="">
-                        </div>
-                        <div class="pl-30">
-                            <h5 class="mb-5 fw-500">
-                                B. Meyers
-                            </h5>
-                            <p class="font-sm text-grey-5">Adobe Jsc</p>
-                            <p class="text-grey-3">"Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
-                                nesciunt voluptatum dicta reprehenderit accusamus voluptatibus voluptas."</p>
-                        </div>
-                    </div>
-                </div>
+
             </div>
 
         </div>
