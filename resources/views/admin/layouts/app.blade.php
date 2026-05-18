58d0ffbfc3f6a1943bbfa96b982ac3149a769543
mdislamrakibul <rakibul.nexkraft@gmail.com>
2026-05-18 01:13:36 -0400

    fic changes

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -60,13 +60,6 @@
 
     </div>
 
-    {{-- Footer --}}
-    @include('admin.layouts.footer')
-
-    </div>
-
-
-
     <script src="{{ asset('admin/plugins/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset('admin/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('admin/dist/js/adminlte.min.js') }}"></script>
